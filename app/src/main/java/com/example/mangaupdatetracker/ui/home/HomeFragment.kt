29d3f2476a2f37f7ba91f696e7d15695be4d2fa5
tursunpulat orolov <tursunpulat2002@gmail.com>
2022-12-26 00:04:23 -0500

    getting info image, titles and chapter links

--- app/src/main/java/com/example/mangaupdatetracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mangaupdatetracker/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mangaupdatetracker.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +9,24 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.mangaupdatetracker.databinding.FragmentHomeBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
+    var TAG = "HomeFragment"
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,8 +37,13 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-
+        binding.testUrlButtonId.setOnClickListener{
+            val urlPage = binding.urlId.text.toString()
+            GlobalScope.launch{
+                val answer = async { getWeb(urlPage) }
+                //.d(TAG, "HTML: ${answer.await()}")
+            }
+        }
 
         return root
     }
@@ -36,5 +51,29 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+
+    suspend fun getWeb(url:String): String {
+        var htmlOutPut = ""
+        try {
+            val doc:Document = Jsoup.connect(url).get()
+            val chapters = doc.select(".panel-story-chapter-list > .row-content-chapter")
+            val aTags = chapters.select("a")
+            val img = doc.select(".story-info-left > .info-image > .img-loading").first()?.attr("src")
+            Log.d(TAG, "IMAGE $img")
+
+            for (a in aTags){
+                val links: String = a.attr("href")
+                val titles: String = a.attr("title")
+                Log.d(TAG, titles)
+                Log.d(TAG, links)
+            }
+
+        }catch (e:Exception){
+            Log.e("shamsi", e.toString())
+        }
+        return htmlOutPut
     }
 }
